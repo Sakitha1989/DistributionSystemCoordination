21f45dbe774ac00d7a72f56c3be15242ccfcdbba
Sakitha1989 <48075532+Sakitha1989@users.noreply.github.com>
2022-02-09 16:28:35 -0600

    power system class complete

--- main.py
+++ main.py
@@ -14,29 +14,45 @@
 import sys
 import os
 
-from readingData import read_files
+from readingData import create_distribution_system, DistributionSystem
 
 import gurobipy as gb
 from gurobipy import GRB, Model, quicksum
 import pandas as pd
 import numpy as np
 
-
+# default inputs if not provided
 directory = "C:\\Users\\sakit\\Documents\\Academic\\Research\\CoordinationSystem\\CoordinationDisSys\\"
-system_name = "Test"
+network_name = "Test"
+system_name = "System1"
+num_systems = 2
 input_dir = ""
 output_dir = ""
 
 
 def cmdinputs() -> None:
-    global directory, system_name, input_dir, output_dir
+    global directory, network_name, system_name, num_systems, input_dir, output_dir
 
-    if not(len(sys.argv) == 1):
-        directory = sys.argv[1]
-        system_name = sys.argv[2]
+    if len(sys.argv) >= 2:
+        if not os.path.exists(sys.argv[1]):
+            print(f"{directory} Directory does not exists!")
+            sys.exit()
+        else:
+            directory = sys.argv[1]
+            input_dir = directory + "inputData\\"
+            if len(sys.argv) >= 3:
+                if not os.path.exists(input_dir + sys.argv[2] + "\\"):
+                    print(f"{network_name} does not exists!")
+                    sys.exit()
+                else:
+                    network_name = sys.argv[2]
+                    input_dir = input_dir + network_name + "\\"
+                if len(sys.argv) >= 4:
+                    system_name = sys.argv[3]
+                    if len(sys.argv) >= 5:
+                        num_systems = int(sys.argv[4])
 
-    input_dir = directory + "inputData\\" + system_name + "\\"
-    output_dir = directory + "outputData\\" + system_name + "\\"
+    output_dir = directory + "outputData\\" + network_name + "\\"
 
     # Create system folder in the output directory if it does not exist
     if not os.path.exists(output_dir):
@@ -46,8 +62,17 @@ def cmdinputs() -> None:
 
 cmdinputs()
 
-file_list = ["genData", "loadData", "busData", "lineData"]
-for file in file_list:
-    read_files(input_dir, file)
-print(input_dir)
-print(system_name)
+i = 1
+while len(os.listdir(input_dir)) >= i:
+
+    distribution_systems = DistributionSystem(system_name + str(i))
+
+    if not os.path.exists(input_dir + system_name + f"{i}\\"):
+        print(f"{system_name + str(i)} does not exists!")
+        sys.exit()
+    else:
+        file_path = input_dir + system_name + f"{i}\\"
+        distribution_systems = create_distribution_system(file_path)
+        if num_systems == i:
+            sys.exit()
+        i += 1
--- readingData.py
+++ readingData.py
@@ -1,8 +1,93 @@
-import pandas as pd
+######################################################################################################################
+# Author:   Sakitha Ariyarathne
+# Date:     02/08/2022
+# Project:  Distribution system coordination
+######################################################################################################################
+
+# Creating all the attributes of the power system.
+
+import csv
 import os
 
 
-def read_files(input_dir, file_name) -> None:
-    gen_data = pd.read_csv(input_dir + file_name, header=True)
-    if not os.path.exists(input_dir + file_name):
-        print(f"{file_name} does not exists!")
+class Generator(object):
+    def __init__(self, attribute_list):
+        self.id = attribute_list['genId']
+        self.bus = attribute_list['genBus']
+        self.cost = attribute_list['genCost']
+        self.active_max = attribute_list['genMaxReal']
+        self.active_min = attribute_list['genMinReal']
+        self.reactive_max = attribute_list['genMaxImagin']
+        self.reactive_min = attribute_list['genMinImagin']
+
+
+class Load(object):
+    def __init__(self, attribute_list):
+        self.id = attribute_list['loadId']
+        self.bus = attribute_list['loadBus']
+        self.active_max = attribute_list['loadMaxReal']
+        self.reactive_max = attribute_list['loadMaxImagin']
+
+
+class Bus(object):
+    def __init__(self, attribute_list):
+        self.id = attribute_list['busId']
+        self.susceptance = attribute_list['busSusceptance']
+        self.conductance = attribute_list['busConductance']
+
+
+class Line(object):
+    def __init__(self, attribute_list):
+        self.id = attribute_list['lineId']
+        self.source = attribute_list['lineSource']
+        self.destination = attribute_list['lineDestination']
+        self.max_flow = attribute_list['lineMaxFlow']
+        self.susceptance = attribute_list['lineSusceptance']
+        self.conductance = attribute_list['lineConductance']
+
+
+class DistributionSystem(object):
+    def __init__(self, system_name):
+        self.name = system_name
+        self.numGenerators = 0
+        self.numLoads = 0
+        self.numBuses = 0
+        self.numLines = 0
+
+        self.generators = []
+        self.loads = []
+        self.buses = []
+        self.lines = []
+
+
+def create_distribution_system(input_dir):
+
+    distribution_system = DistributionSystem('Distribution_system_1')
+
+    # list of file names to read
+    file_list = ["genData", "loadData", "busData", "lineData"]
+
+    for file_name in file_list:
+        if not os.path.exists(input_dir + file_name + ".csv"):
+            print(f"{file_name} does not exists!")
+        else:
+            with open(input_dir + file_name + ".csv", 'r') as file:
+                data = csv.DictReader(file)
+                if file_name == "genData":
+                    for row in data:
+                        distribution_system.generators.append(Generator(row))
+                        distribution_system.numGenerators += 1
+                elif file_name == "loadData":
+                    for row in data:
+                        distribution_system.loads.append(Load(row))
+                        distribution_system.numLoads += 1
+                elif file_name == "busData":
+                    for row in data:
+                        distribution_system.buses.append(Bus(row))
+                        distribution_system.numBuses += 1
+                if file_name == "lineData":
+                    for row in data:
+                        distribution_system.lines.append(Line(row))
+                        distribution_system.numLines += 1
+
+    return distribution_system
